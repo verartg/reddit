8b6a4f18eda775d3c54725626d74d6317fcc7b36
verartg <verarottger@gmail.com>
2022-12-14 15:50:40 +0100

    routing opgezet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from '../src/pages/home/Home';
+import Subreddit from '../src/pages/subreddit/Subreddit';
 import './App.css';
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/subreddit/:subredditId" element={<Subreddit/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/subreddit/Subreddit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Subreddit() {
+    return (
+        <div>blabla</div>
+    );
+}
+
+export default Subreddit;
